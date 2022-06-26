f5b2702af3e9834caa6ef311284bcc50feade0d5
Agustinus Kurniawan <agustinus93@gmail.com>
2022-06-27 00:00:28 +0700

    Challenge Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+import contacts from './data/contacts.json';
+import Contact from './components/Contact';
+import Header from './components/Header';
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
 
@@ -7,7 +9,10 @@ const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+            <Header />
+      <div id='contact-list'>
+        { contacts.map(contact => <Contact key={contact.email} data={contact} />) }
+      </div>
     </div>
   );
 }
